c0b5edd527fd92bc395ce0f70602ec79f2b20b8c
Ruben Lopez <ruben@cs1.com>
2013-01-23 03:33:24 -0800

    Site is up and running

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journalv2::Application.config.secret_token = 'bd09b1f15dbe718dba7af6d5d7bd61fad7a4e8b95a15a5a281e85e73080021c5fb36656e800ec09a0871fb9b954261d8679b48f53dbfc336a66b8d9ccd2d04b7'
